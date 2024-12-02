07484e74fc8515bd9773915e66d89ddab524803a
bastien-benis <bastien.benis@etudiant.univ-rennes1.fr>
2024-12-02 18:13:44 +0100

    commande avec zqsd

--- Client.py
+++ Client.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
-from mrpiZ_lib import
+from mrpiZ_lib import *
 import time
+
+# seuil de la detection obstacle
+seuil = 50
+
 while 1:
-p1: proxSensor(1)
-p2:proxSensor(2)
-p3:proxSensor(3)
-print "%d %d %d" %(p1,p2,p3)
+    p2 = proxSensor(2)
+    p4 = proxSensor(4)
+    print "%d %d " %(p2,p4)
 
-time.sleep(1)
+    if ((p2 < seuil) or (p4 < seuil)):
+        stop() # arreter le robot
+    else:
+        forward(30) # avancer le robot
 
+    time.sleep(0.2)
--- Serveur.py
+++ Serveur.py
@@ -2,6 +2,7 @@
 from mrpiZ_lib import *
 import time, os
 from typing import Tuple
+import keyboard
 
 A_GAUCHE : int = 1
 A_DROITE : int = -1
@@ -12,41 +13,48 @@ ARRIERE : int = 1
 VITESSE_MAX:int = 100
 VITESSE_MIN:int = 25
 
-if __name__ == "__main__":
-    vitesse_gauche:int
-    vitesse_droite:int
-    sens_gauche:int
-    sens_droit:int
-    fin:bool = False
-    try:
-        vitesse_gauche = VITESSE_MAX
-        vitesse_droite = VITESSE_MAX
-        sens_gauche = AVANT
-        sens_droit = AVANT
-
-        motorLeft(sens_gauche, vitesse_gauche)
-        motorRight(sens_droit, vitesse_droite)
-        time.sleep(2)
-
-        sens_gauche = ARRIERE
-        sens_droit = ARRIERE
-        motorLeft(sens_gauche, vitesse_gauche)
-        motorRight(sens_droit, vitesse_droite)
-        time.sleep(2)
-
-        sens_gauche = AVANT
-        sens_droit = ARRIERE
-        motorLeft(sens_gauche, vitesse_gauche)
-        motorRight(sens_droit, vitesse_droite)
-        time.sleep(2)
-
-        sens_gauche = ARRIERE
-        sens_droit = AVANT
-        motorLeft(sens_gauche, vitesse_gauche)
-        motorRight(sens_droit, vitesse_droite)
+class robot:
+    def __init__(self):
+        self.vitesse_gauche:int = VITESSE_MAX
+        self.vitesse_droite:int = VITESSE_MAX
+        self.sens_gauche:int = AVANT
+        self.sens_droit:int = AVANT
         
-        time.sleep(2)
+    def avancer(self):
+        self.sens_gauche = AVANT
+        self.sens_droit = AVANT
+        motorLeft(self.sens_gauche, self.vitesse_gauche)
+        motorRight(self.sens_droit, self.vitesse_droite)
 
-        stop()
-    except KeyboardInterrupt as ex:
-        stop()
+    def reculer(self):
+        self.sens_gauche = ARRIERE
+        self.sens_droit = ARRIERE
+        motorLeft(self.sens_gauche, self.vitesse_gauche)
+        motorRight(self.sens_droit, self.vitesse_droite)
+
+    def tourner_gauche(self):
+        self.sens_gauche = ARRIERE
+        self.sens_droit = AVANT
+        motorLeft(self.sens_gauche, self.vitesse_gauche)
+        motorRight(self.sens_droit, self.vitesse_droite)
+
+    def tourner_droite(self):
+        self.sens_gauche = AVANT
+        self.sens_droit = ARRIERE
+        motorLeft(self.sens_gauche, self.vitesse_gauche)
+        motorRight(self.sens_droit, self.vitesse_droite)
+        
+
+if __name__ == "__main__":
+    Robot = robot()
+    
+    while True:
+        if keyboard.is_pressed('z'):
+            robot.avancer()
+        elif keyboard.is_pressed('s'):
+            robot.reculer()
+        elif keyboard.is_pressed('q'):
+            robot.tourner_gauche()
+        elif keyboard.is_pressed('d'):
+            robot.tourner_droite()
+        time.sleep(0.1)
