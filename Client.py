098ef61c717cc9dc4729422a49e3050565a1c70b
lebuilder <76538612+lebuilder@users.noreply.github.com>
2024-12-09 08:13:53 +0100

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,18 +1,80 @@
-#!/usr/bin/python
-from mrpiZ_lib import *
-import time
 
-# seuil de la detection obstacle
-seuil = 50
+from tkinter import *
 
-while 1:
-    p2 = proxSensor(2)
-    p4 = proxSensor(4)
-    print "%d %d " %(p2,p4)
+class Fen_Principale(Tk):
+    def __init__(self)-> None:
+        Tk.__init__(self)
+        # déclaration
+        self.__addr:str
+        self.__port:int
+        self.__lbl_adr_port:Label
+        self.__btn_config:Button
+        self.__btn_init:Button
+        self.__btn_quitter:Button
+        # instanciation / initialisation
+        self.title("fenetre principale")
+        self.__lbl_adr_port = Label(self)
+        self.__btn_config = Button(self, text="configuration", command= lambda : Fen_Config(self))               
+        self.__btn_init = Button(self,text="initialisation",command=self.init)
+        self.__btn_quitter = Button(self,text="Quitter",bg="red",command=self.destroy )
+        self.init() # premier appel pour intialisation de l'adresse
 
-    if ((p2 < seuil) or (p4 < seuil)):
-        stop() # arreter le robot
-    else:
-        forward(30) # avancer le robot
+        # ajout des widgets
+        self.__lbl_adr_port.pack()
+        self.__btn_config.pack()
+        self.__btn_init.pack()
+        self.__btn_quitter.pack()
 
-    time.sleep(0.2)
+    #modificateur
+    def set_addr(self,addr:str)->None:
+        self.__addr = addr
+    def set_port(self,port:int)->None:
+        self.__port = port
+    def set_lbl_adr_port(self)->None:
+        self.__lbl_adr_port["text"]=f"serveur {self.__addr}:{str(self.__port)}"
+    def init(self)->None:
+      self.__lbl_adr_port["text"]="serveur xxx.xxx.xxx.xxx : XXXX"
+
+class Fen_Config(Toplevel):
+    def __init__(self, fenP:Fen_Principale)-> None:
+        Toplevel.__init__(self)
+        self.__fenP = fenP# memorisation de la fenètre principale, accès aux méthodes
+        # declaration
+        self.__lbl_adr:Label
+        self.__entree_adr:Entry
+        self.__lbl_port:Label
+        self.__entree_port:Entry
+        self.__btn_retour:Button
+
+        # instantiation / initialisation
+        self.__fenP.withdraw() # effacer fenetre principale
+        self.title("config")
+        self.__lbl_adr = Label(self, text="adr serveur")
+        self.__entree_adr = Entry(self,width=15)
+        self.__entree_adr.insert(0,"127.0.0.1")
+        self.__lbl_port = Label(self,text="port serveur")
+        self.__entree_port = Entry(self,width= 5)
+        self.__entree_port.insert(0,"5000")
+        self.__btn_retour = Button(self,text="Retour", command= self.configuration)
+        # ajout des widgets
+        self.__lbl_adr.grid(row= 0,column= 0)
+        self.__entree_adr.grid(row=0,column= 1)
+        self.__lbl_port.grid(row=1,column= 0)
+        self.__entree_port.grid(row=1,column= 1)
+        self.__btn_retour.grid(row=2,column=0)
+        # evenements
+        self.protocol("WM_DELETE_WINDOW", self.configuration)
+
+    def configuration(self)-> None:
+        self.__fenP.set_addr(self.__entree_adr.get())
+        self.__fenP.set_port(int(self.__entree_port.get()))
+        self.__fenP.set_lbl_adr_port()
+
+
+        self.__fenP.deiconify() # afficher la fenetre principale
+        self.destroy() # detruire la fenetre courante
+
+
+if __name__ == "__main__":
+    fen_principale:Fen_Principale = Fen_Principale()
+    fen_principale.mainloop()
--- Serveur.py
+++ Serveur.py
@@ -1,60 +1,68 @@
-#!/usr/bin/python
-from mrpiZ_lib import *
-import time, os
-from typing import Tuple
-import keyboard
+from socket import *
+import sys
 
-A_GAUCHE : int = 1
-A_DROITE : int = -1
-
-AVANT : int = 0
-ARRIERE : int = 1
-
-VITESSE_MAX:int = 100
-VITESSE_MIN:int = 25
-
-class robot:
-    def __init__(self):
-        self.vitesse_gauche:int = VITESSE_MAX
-        self.vitesse_droite:int = VITESSE_MAX
-        self.sens_gauche:int = AVANT
-        self.sens_droit:int = AVANT
+class ServiceEcoute :
+    def __init__(self, port_serveur: int) -> None:
+        self.__socket_ecoute:socket = socket(AF_INET, SOCK_STREAM)
+        self.__socket_ecoute.bind(('', port_serveur))
+        self.__socket_ecoute.listen(1)
+        print(f"écoute sur le port :  {port_serveur}")
         
-    def avancer(self):
-        self.sens_gauche = AVANT
-        self.sens_droit = AVANT
-        motorLeft(self.sens_gauche, self.vitesse_gauche)
-        motorRight(self.sens_droit, self.vitesse_droite)
-
-    def reculer(self):
-        self.sens_gauche = ARRIERE
-        self.sens_droit = ARRIERE
-        motorLeft(self.sens_gauche, self.vitesse_gauche)
-        motorRight(self.sens_droit, self.vitesse_droite)
-
-    def tourner_gauche(self):
-        self.sens_gauche = ARRIERE
-        self.sens_droit = AVANT
-        motorLeft(self.sens_gauche, self.vitesse_gauche)
-        motorRight(self.sens_droit, self.vitesse_droite)
-
-    def tourner_droite(self):
-        self.sens_gauche = AVANT
-        self.sens_droit = ARRIERE
-        motorLeft(self.sens_gauche, self.vitesse_gauche)
-        motorRight(self.sens_droit, self.vitesse_droite)
-        
-
-if __name__ == "__main__":
-    Robot = robot()
+    def attente(self) -> socket:
+        print("Attente d'une connexion ... ")
+        client_socket, client_address = self.__socket_ecoute.accept()
+        print(f"connexion avec le client : {client_address}")
+        return client_socket
     
-    while True:
-        if keyboard.is_pressed('z'):
-            robot.avancer()
-        elif keyboard.is_pressed('s'):
-            robot.reculer()
-        elif keyboard.is_pressed('q'):
-            robot.tourner_gauche()
-        elif keyboard.is_pressed('d'):
-            robot.tourner_droite()
-        time.sleep(0.1)
+class ServiceEchange :
+    def __init__(self, socket_echange:socket) -> None:
+        self.__socket_echange = socket_echange
+    
+    def envoyer(self, msg:str) -> None:
+        self.__socket_echange.send(msg.encode('utf-8'))
+        
+        
+    def recevoir(self) -> str:
+        tab_octets = self.__socket_echange.recv(1024)
+        return tab_octets.decode(encoding='utf-8')
+
+    
+    def echange(self)->None:
+        fin:bool =False
+        while(not fin):
+
+            tab_octets = self.__socket_echange.recv(1024)
+
+            msg_client = tab_octets.decode(encoding="utf-8")
+            print("message du client :", msg_client)
+
+            msg_serveur:str = f"**{msg_client}**"
+
+            if msg_client == "fin":
+                fin=True
+
+            tab_octets = msg_serveur.encode(encoding="utf-8")
+            self.__socket_echange.send(tab_octets)
+    
+    
+    def arret(self) -> None:
+        self.__socket_echange.close()
+    
+if __name__=="__main__":
+    # declaration des variables
+    port_ecoute: int=None
+    service_ecoute: ServiceEcoute=None
+    socket_client: socket=None
+    service_echange: ServiceEchange=None
+    # lecture des parametres (le numero de port)
+    if len(sys.argv)==2:
+        port_ecoute=int(sys.argv[1])
+    else:
+        port_ecoute=5000
+    try:
+        service_ecoute=ServiceEcoute(port_ecoute)
+        socket_client=service_ecoute.attente()
+        service_echange=ServiceEchange(socket_client)
+        service_echange.echange()
+    except Exception as ex:
+        print("erreur : ", ex)
